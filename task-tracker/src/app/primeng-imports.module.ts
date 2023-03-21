8883da07d9d5ba5c74d1f4114e2127e20fbc0bd9
Peachess <annasamokaeva2020@gmail.com>
2023-03-21 21:40:09 +0200

    init: start project

---
+++ task-tracker/src/app/primeng-imports.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { TableModule } from 'primeng/table';
+
+@NgModule({
+  exports: [ButtonModule, InputTextModule, TableModule],
+})
+export class PrimengModule {}
