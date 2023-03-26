c2dada70a28937450ee8b664a8fce6a085b3a62b
Peachess <annasamokaeva2020@gmail.com>
2023-03-26 19:28:13 +0300

    feat: implement welcome page

--- task-tracker/src/app/app.module.ts
+++ task-tracker/src/app/app.module.ts
@@ -4,9 +4,12 @@ import { PrimengModule } from './primeng-imports.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { WelcomePageComponent } from './components/welcome-page/welcome-page.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, WelcomePageComponent],
   imports: [BrowserModule, AppRoutingModule, PrimengModule],
   providers: [],
   bootstrap: [AppComponent],
